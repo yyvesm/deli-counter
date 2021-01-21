c05b09b9d17804bb2e2e1664ff00582f4642b01a
yves <yvesmomplais@hotmail.com>
2021-01-21 17:37:10 -0500

    commit tree 2bea9b3a7f944abd8598b0e6f1151e6c8a4982cb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+katz_deli = [] 
+
+def line(katz_deli) 
+   position = 1 
+   queue = []
+
+ if katz_deli.length == 0     
+    puts "The line is currently empty."   
+ else 
+    katz_deli.each do |customer|     
+    queue.push("#{position}. #{customer}")     
+    position +=1   
+ end   
+ puts "The line is currently: #{queue.join(" ")}"   
+ end 
+end
+
+
+
+def take_a_number(katz_deli, customer)   
+    katz_deli.push(customer)   
+    puts "Welcome, #{customer}. You are number #{katz_deli.count} in line." 
+ end  
+ 
+
+
+ def now_serving(katz_deli)   
+    if katz_deli.empty?     
+       puts "There is nobody waiting to be served!"   
+    else     
+       puts "Currently serving #{katz_deli.first}."   
+       katz_deli.shift 
+    end 
+ end
